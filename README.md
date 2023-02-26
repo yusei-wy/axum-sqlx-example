7f3efd88d692c6f8af2c5df1d97c9d18e5ea0513
yusei-wy <yusei.kasa@gmail.com>
2023-02-26 23:48:48 +0900

    add: create user

--- README.md
+++ README.md
@@ -16,3 +16,10 @@ axum x sqlx
 | `make build` | PostgreSQL DB を docker 上で起動する |
 | `make add-migrate NAME=<name>` | Migration ファイルを追加 |
 | `make serve` | cargo-watch で監視した状態で `cargo run` する |
+
+
+## API
+
+| path | method |  request |
+| --- | --- | --- |
+| /api/users | POST | `curl -X POST -H "Content-Type: application/json" -d '{"nickname": "", "birthday": "1993-01-01"}' localhost:3000/api/users` |
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
-use axum::{http::StatusCode, response::IntoResponse, routing::get, Extension, Router};
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
 use chrono::{NaiveDate, NaiveDateTime};
 use dotenv::dotenv;
-use serde::Serialize;
-use sqlx::{self, Connection, PgConnection};
-use std::{env, net::SocketAddr, sync::Arc};
-use uuid::Uuid;
+use serde::{Deserialize, Serialize};
+use sqlx::{postgres::PgPoolOptions, types::uuid::Uuid, PgPool};
+use std::{env, net::SocketAddr};
 
-#[derive(Serialize)]
+#[derive(Serialize, Clone)]
 struct User {
     user_id: Uuid,
     nickname: String,
     birthday: NaiveDate,
+}
+
+#[derive(Deserialize)]
+struct CreateUserInput {
+    nickname: String,
+    birthday: String,
 }
 
 #[derive(Serialize)]
@@ -23,27 +34,65 @@ struct Todo {
     edited_at: NaiveDateTime,
 }
 
-async fn health_check() -> impl IntoResponse {
-    StatusCode::NO_CONTENT
-}
-
 async fn home() -> &'static str {
     "Hello World"
 }
 
+async fn health_check() -> impl IntoResponse {
+    StatusCode::NO_CONTENT
+}
+
+async fn create_user(
+    State(pool): State<PgPool>,
+    Json(payload): Json<CreateUserInput>,
+) -> (StatusCode, Json<User>) {
+    let birthday = NaiveDate::parse_from_str(&payload.birthday, "%Y-%m-%d").unwrap();
+
+    let new_user = User {
+        user_id: Uuid::new_v4(),
+        nickname: payload.nickname,
+        birthday,
+    };
+    let result_user = new_user.clone();
+
+    sqlx::query(
+        r#"
+        INSERT INTO users (user_id, nickname, birthday) VALUES ($1, $2, $3)
+        "#,
+    )
+    .bind(new_user.user_id)
+    .bind(new_user.nickname)
+    .bind(new_user.birthday)
+    .execute(&pool)
+    .await
+    .ok();
+
+    (StatusCode::CREATED, Json(result_user))
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<(), sqlx::Error> {
     dotenv().ok();
 
     let db_url = env::var("DATABASE_URL").unwrap();
-    let conn = PgConnection::connect(&db_url).await.unwrap();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await?;
+
     let app = Router::new()
         .route("/", get(home))
         .route("/health", get(health_check))
-        .layer(Extension(Arc::new(conn)));
+        .route("/api/users", post(create_user))
+        .with_state(pool.clone());
+
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    pool.close().await;
+    Ok(())
 }
